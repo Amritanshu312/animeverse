b9514e8921017c4fb82db40c88606c60ff1d9f2d
Amritanshu312 <ffdarkrayon@gmail.com>
2024-03-03 19:24:08 +0800

    fixed: banner video loading slowly

--- components/ui/banner/Banner.jsx
+++ components/ui/banner/Banner.jsx
@@ -4,7 +4,6 @@ import styles from "./banner.module.css"
 import AnimeInfo from "./AnimeInfo/AnimeInfo"
 import { useState } from "react"
 import VideoPlay from "./videoPlay/VideoPlay"
-import { Suspense } from 'react'
 
 const Banner = ({ info }) => {
   const [isVideoPlaying, setIsVideoPlaying] = useState(false)
@@ -28,7 +27,7 @@ const Banner = ({ info }) => {
     >
       <div className={styles.bannerImage}>
         {!isVideoPlaying || !info?.video || !isImageLoaded ? <Image src={info.image} alt="banner" width={1000} height={396} onLoad={() => setIsImageLoaded(true)} /> : null}
-        {info?.video && isVideoPlaying ? <VideoPlay url="https://firebasestorage.googleapis.com/v0/b/anime-nsfw.appspot.com/o/edits%2Fdemon%20slayer%20edit%20(2).mp4?alt=media&token=dd51ecd4-2dcd-4ce8-b783-3612aea528f6" play={isVideoPlaying} /> : null}
+        {info?.video && isVideoPlaying ? <VideoPlay url={info?.video} play={isVideoPlaying} /> : null}
       </div>
 
       <AnimeInfo info={info} />
