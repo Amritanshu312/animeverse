a8d7b052b7a3784904d8d9d971f2e189f51baf27
Amritanshu312 <ffdarkrayon@gmail.com>
2024-03-04 19:51:26 +0800

    clean: info layout page

--- app/(home)/info/[id]/layout.js
+++ app/(home)/info/[id]/layout.js
@@ -1,45 +1,4 @@
-// export const metadata = {
-//   title: "Animeverse || the best",
-//   description: "Welcome to Animeverse – your ultimate anime destination! Stream your favorite anime titles in sync with friends and explore a vast library of series and movies. Join our vibrant community for an unforgettable anime-watching experience!",
-// };
-
-import { fetchData } from "@/lib/FetchData";
-
-export async function generateMetadata({ params, searchParams }, parent) {
-  try {
-    const responseData = await fetchData(`/meta/anilist/minimalinfo/${params.id}`);
-
-    if (!responseData || !responseData.data) {
-      throw new Error("No data received from the server.");
-    }
-
-    const data = responseData.data;
-
-    if (!data.title || !data.title.english) {
-      throw new Error("Title information not available in the fetched data.");
-    }
-
-    return {
-      metadataBase: new URL('http://localhost:3000'),
-      title: `Animeverse - ${data.title.english}`,
-      description: data.description || "No description available.",
-      openGraph: {
-        images: [data.image],
-      },
-    };
-  } catch (error) {
-    console.error("Error fetching or processing data:", error);
-    return {
-      title: "Error Occurred",
-      // openGraph: {
-      //   images: ['/error-image.jpg'],
-      // },
-    };
-  }
-}
-
-
-export default function RootLayout({ children, params }) {
+export default function RootLayout({ children }) {
   return (
     children
   );
