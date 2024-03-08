b74a3dee387581d70b8d12c9065128a4ce1900ac
Amritanshu312 <ffdarkrayon@gmail.com>
2024-03-08 23:18:23 +0800

    fixed: home page not able to acces Content/Home folder

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -1,7 +1,7 @@
 import AtoZalphabet from "@/components/ui/AtoZalphabet/AtoZalphabet";
-import Discover from "@/content/Home/discover/Discover";
-import RecentRelease from "@/content/Home/recentRelease/RecentRelease";
-import SlidingBanner from "@/content/Home/slidingBanner/SlidingBanner";
+import Discover from "@/content/HomePage/Home/discover/Discover";
+import RecentRelease from "@/content/HomePage/Home/recentRelease/RecentRelease";
+import SlidingBanner from "@/content/HomePage/Home/slidingBanner/SlidingBanner";
 
 export default function Home() {
   return <>
