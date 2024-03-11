76a39499d4f4cf51e71292450429f9b09b5388bf
Amritanshu312 <ffdarkrayon@gmail.com>
2024-03-11 20:18:11 +0800

    added: google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
