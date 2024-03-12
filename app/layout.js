c2012570f8cf023496f15ef0b991e24a86e98398
Amritanshu312 <ffdarkrayon@gmail.com>
2024-03-12 16:09:15 +0800

    added: search console

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Animeverse",
   description: "Welcome to Animeverse – your ultimate anime destination! Stream your favorite anime titles in sync with friends and explore a vast library of series and movies. Join our vibrant community for an unforgettable anime-watching experience!",
+  robots: { index: false, follow: false },
+  verification: { google: 'ls1OUoOoLjxYsmKMPQ1ML9P99TWDsm7d5hfnGQjW7Tw', }
 };
 
 export default function RootLayout({ children }) {
